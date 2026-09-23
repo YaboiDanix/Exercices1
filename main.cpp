f1d900ea3c848d044d75afaed360a1f0a820506b
YaboiDanix <Danixz1255@gmail.com>
2026-09-23 12:02:07 +0200

    exercices 1.2

--- main.cpp
+++ main.cpp
@@ -4,12 +4,21 @@
 
 
 int main() {
-   int pv = 42 ;
+    int pv = 42 ;
     std::println("PV : {}", pv);
-
+    //exercice 1.1
     if (pv == 0) {
         std::println("game over");
     }
+    //exercice 1.2
+    int munitions = 7;
+    if (munitions % 2) {
+        std::println("munitions est impair");
+    }
+    else
+    {
+        std::println("munitions est pair");
+}
 
 
 
