dfb3c27c4ab5c4f738fc0dd9004643eae7e94491
YaboiDanix <Danixz1255@gmail.com>
2026-09-23 11:50:00 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <print>
+
+
+int main() {
+   int pv = 42 ;
+    std::printf("PV : {}", pv);
+
+    if (pv = 0) {
+        std::printf("game over");
+    }
+
+
+
+
+    return 0;
+}
